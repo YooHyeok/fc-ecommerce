cbdc781ccc2551ef103450704b61dbf2a55f7b8e
webdevyoo <yjk7454@naver.com>
2025-01-23 00:30:40 +0900

    feat: Ch05. 최저가 서비스 zadd 상품 그룹 추가 API 구현

--- module-application/src/main/java/com/fc/moduleapplication/controller/LowestPriceController.java
+++ module-application/src/main/java/com/fc/moduleapplication/controller/LowestPriceController.java
@@ -2,6 +2,7 @@ package com.fc.moduleapplication.controller;
 
 import com.fc.moduleapplication.service.LowestPriceService;
 import com.fc.moduleapplication.vo.Product;
+import com.fc.moduleapplication.vo.ProductGroup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +20,7 @@ public class LowestPriceController {
 
     private final LowestPriceService lowestPriceService;
     /**
-     * Redis 상품 데이터 조회
+     * <h5>Redis 상품 데이터 조회</h5>
      * <pre>
      *      ProductGroupId key 값을 기준으로 조회한다.
      *      0위부터 9위까지 범위로 10개의 product를 조회한다.
@@ -34,16 +35,27 @@ public class LowestPriceController {
     }
 
     /**
-     * Redis 상품 데이터 추가 및 추가된 상품 랭킹 반환
+     * <h5>Redis 상품 데이터 추가 및 추가된 상품 랭킹 반환</h5>
      * <pre>
      *      ProductGroupId, ProductId, Price 정보를 추가한다.
-     *      ProductGroupId
+     *      ProductGroupId, ProductId 기준 Price의 순위를 조회하여 반환한다.
+     *
+     *      PUT 방식: 멱등성 - 없으면 새로 추가 있으면 갱신
+     *      ZSET의 중복을 허용하지 않는 특성으로 인해 PUT 방식을 사용한다.
+     *      (동일한 MEMBER가 이미 있다면, 해당 MEMBER에 새로운 SCORE를 덮어씌운다.)
+     *      PUT 방식의 멱등성 자바의 Map 자료구조의 put의 의미와 같다고 생각하자.
+     *      (Map도 key가 있다면 해당 key에 새로운 value를 덮어씌운다.)
      * </pre>
      * @param product
      * @return int ()
      */
-    @PostMapping("/product")
+    @PutMapping("/product")
     public int setNewProduct(@RequestBody Product product) {
         return lowestPriceService.setNewProduct(product);
+    }
+
+    @PutMapping("/productGroup")
+    public int setNewProductGroup(@RequestBody ProductGroup productGroup) {
+        return lowestPriceService.setNewProductGroup(productGroup);
     }
 }
